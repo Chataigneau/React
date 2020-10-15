7abb3d80448c62c2b0ec0d51cfc7a731abca8fa1
Nico <skywosh@gmail.com>
2020-10-15 13:59:13 +0200

    Colors

--- src/features/Contact/Contact.js
+++ src/features/Contact/Contact.js
@@ -3,7 +3,7 @@ import {Footer,Anchor, Text} from "grommet"
 
 const Contact = () => {
     return(
-        <Footer background="neutral-3" pad="medium">
+        <Footer background="white" pad="medium">
             <Text>Copyright</Text>
             <Anchor label="Nicolas Chataigneau" href="https://www.linkedin.com/in/nicolas-chataigneau-838a981a6/"/>
             <Text>nicolas.chataigneau@ynov.com</Text>
